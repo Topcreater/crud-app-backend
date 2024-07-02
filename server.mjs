8972c46d88e50f1674ede7f3a6a42fc56ed45ef5
Topcreater <mubashar7964768@gmail.com>
2024-07-02 17:02:44 +0500

    origin change

--- server.mjs
+++ server.mjs
@@ -7,26 +7,33 @@ import financialRoutes from './routes/financialRoutes.mjs';
 import customerRoutes from './routes/customerRoutes.mjs';
 import contractRoutes from './routes/contractRoutes.mjs';
 import cors from 'cors';
+
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI; // Use MongoDB URI from environment variables
 
+// Middleware
 app.use(express.json());
-app.use(cors({
-    origin: '*', // Allow requests from this origin
-    methods: ['GET', 'POST' , 'PUT' , 'DELETE', 'PATCH'], // Allow these HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-  }));
-  app.get('/api', (req, res) => {
-    res.send('Hello World from Vercel!');
-  });
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Routes
+app.get('/api', (req, res) => {
+  res.send('Hello World from Vercel!');
+});
+
+// Mounting routes
 app.use('/auth', authRoutes);
 app.use('/financial', financialRoutes);
 app.use('/customer', customerRoutes);
 app.use('/contract', contractRoutes);
 
+// Connect to MongoDB and start server
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
